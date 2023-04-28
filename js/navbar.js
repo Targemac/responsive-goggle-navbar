5384fe97d9f2112cd6d00ab9d5c5b3fdb0e05398
Targemac <targemac@gmail.com>
2023-04-28 12:48:03 +0100

    Completed!

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  //   window.alert("Hi Dami");
+  $("#toggleBtn").click(function () {
+    $("#mobile_menu").slideToggle();
+    // $("#mobile_menu").fadeToggle();
+  });
+
+  //   $("#toggleBtn").mouseenter(function () {
+  //     $("#mobile_menu").slideDown();
+  //     // $("#mobile_menu").fadeToggle();
+  //   });
+
+  //   $("#toggleBtn").mouseleave(function () {
+  //     $("#mobile_menu").slideUp();
+  //     // $("#mobile_menu").fadeToggle();
+  //   });
+});
